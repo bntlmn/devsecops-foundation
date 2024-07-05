b90bea4948111ad6df476127d3e1f135dd792970
EC2 Default User <ec2-user@ip-172-31-81-181.ec2.internal>
2024-07-05 03:59:44 +0000

    New commit for limited files

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "app_server_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = "i-0413e8e323968a259"
+}
+
+output "app_server_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = "18.138.251.247"
+}
